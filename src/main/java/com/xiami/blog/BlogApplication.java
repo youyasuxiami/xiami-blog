83e972dbb831c4c12fc03bb7aaa6163c111e289c
yuan-jia-er <739356032@qq.com>
2020-05-14 00:14:50 +0800

    用mybatisCodeHelper插件生成基操，写了测试类，并且在启动类上面添加了注解

--- src/main/java/com/xiami/blog/BlogApplication.java
+++ src/main/java/com/xiami/blog/BlogApplication.java
@@ -2,8 +2,10 @@ package com.xiami.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+@MapperScan("com.xiami.blog.dao")
 public class BlogApplication {
 
     public static void main(String[] args) {
